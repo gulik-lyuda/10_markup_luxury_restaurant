3114b6ce3e3650f88331f8ec0523f0ba604a2b5b
Lyudmyla <glyuda@yahoo.com>
2017-12-18 00:19:28 +0200

    Adding a markup of the Happy Clients section

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,17 @@
-/**
- * Created by lyuda on 04.05.2017.
- */
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  direction: 'vertical',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
